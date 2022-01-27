49e4d7358c955931cb244ef9c3b0af0946b6b174
smartlid <lidavidwork@gmail.com>
2022-01-26 20:32:45 -0800

    Add airtable table name to env

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
   env: {
     AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
     AIRTABLE_BASE_KEY: process.env.AIRTABLE_BASE_KEY,
+    AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
   }
 }
