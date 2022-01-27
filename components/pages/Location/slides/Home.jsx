e817d5d2153f4ea3706971641397064c6162eb81
smartlid <lidavidwork@gmail.com>
2022-01-27 10:28:14 -0800

    Complete style of the first home page

--- components/pages/Location/slides/Home.jsx
+++ components/pages/Location/slides/Home.jsx
@@ -20,15 +20,17 @@ export default function Home() {
     }
   }, [record])
 
-  init();
+  // init();
 
   return (
     <div className={`${slideStyles.slide} ${slideStyles["slide-home"]} ${slideStyles["bg-light"]}`}>
-      <img src="/assets/remote-roofing-logo.png" alt="logo" className={`${slideStyles["home-logo"]} fade-in-top-down`} />
-      <h1 className={`${slideStyles["home-title"]} fade-in-top-down`}><span style={{ color: '#075BBD' }}>AI</span> Roof Analysis</h1>
-      <h3 className={`${slideStyles["home-address"]} fade-in-top-down`}>{address}</h3>
+      <img src="/assets/remote-roofing-logo.png" alt="logo" className={`${slideStyles["home__logo"]} fade-in-top-down`} />
+      <h1 className={`${slideStyles["home__title"]} fade-in-top-down`}><span style={{ color: '#075BBD' }}>AI</span> Roof Analysis</h1>
+      <h3 className={`${slideStyles["home__address"]} fade-in-top-down`}>{address}</h3>
 
-      <primitive object={obj} />
+      <div className={`${slideStyles["home__map-area"]} fade-in-top-down`}>
+        <img src="/assets/drone.png" alt="drone" className={`${slideStyles["home__drone"]} fade-in-top-down`} />
+      </div>
     </div>
   );
 }
