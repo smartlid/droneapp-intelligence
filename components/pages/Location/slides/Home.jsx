93004c862eefdea5ba466576f954109dfc1fe7b1
smartlid <lidavidwork@gmail.com>
2022-01-27 10:57:09 -0800

    Remove init function

--- components/pages/Location/slides/Home.jsx
+++ components/pages/Location/slides/Home.jsx
@@ -2,25 +2,15 @@ import { useEffect, useState } from "react";
 import { useAppContext } from "../../../../pages/_app";
 import slideStyles from "./styles.module.scss"
 
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { useLoader } from '@react-three/fiber'
-
 export default function Home() {
   const { record } = useAppContext();
   const [address, setAddress] = useState('');
-  const [obj, setObj] = useState(null);
-
-  const init = () => {
-    const obj = useLoader(OBJLoader, '../../../../public/assets/texas-map-3d.obj');
-  }
 
   useEffect(() => {
     if (record) {
       setAddress(record.fields['Property Address']);
     }
   }, [record])
-
-  // init();
 
   return (
     <div className={`${slideStyles.slide} ${slideStyles["slide-home"]} ${slideStyles["bg-light"]}`}>
