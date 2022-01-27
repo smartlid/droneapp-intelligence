c71c91fd21f07f42b7b456f4b3145236fb04ecc9
smartlid <lidavidwork@gmail.com>
2022-01-26 20:29:15 -0800

    Build home page component and get data from airtable

---
+++ components/pages/Location.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Airtable from "airtable";
+
+export default function Location() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { id } = router.query
+    if (id) {
+      const recordKey = id[0];
+      const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY);
+      base('Table 1').find(recordKey, (err, record) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log('Received: ', record);
+      });
+    }
+  }, [router.query]);
+
+  return <div>dfsfdsd</div>;
+}
