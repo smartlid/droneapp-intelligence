0d5c6047e80e7e1bb3195fccba7fbf031e1230b0
smartlid <lidavidwork@gmail.com>
2022-01-30 20:48:57 -0800

    Build about page

--- components/pages/slides/About.jsx
+++ components/pages/slides/About.jsx
@@ -1,14 +1,52 @@
-import { useEffect } from "react"
-import slideStyles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
-export default function About({ setPaginationClass }) {
-  useEffect(() => {
-    setPaginationClass('light');
-  }, [setPaginationClass])
-
+export default function About() {
   return (
-    <div className={`${slideStyles.slide}`}>
-      <img src="/assets/about.png" width="100%" />
+    <div
+      className={`${styles.slide} ${styles["slide-about"]} ${styles["bg-light"]}`}
+    >
+      <img
+        src="/assets/remote-roofing-logo.png"
+        alt="logo"
+        className={`${styles.logo} fade-in-top-down`}
+      />
+      <img src="/assets/drone1.png" style={{ position: "absolute", left: "18%", top: "35%" }} />
+      <img src="/assets/drone2.png" style={{ position: "absolute", right: "25%", top: "45%" }} />
+
+      <div className={styles.content}>
+        <div style={{ textAlign: "center", marginTop: "10%" }}>
+          <h3
+            className="fade-in-top-down"
+            style={{
+              fontSize: "30px",
+              margin: 0,
+              opacity: 0,
+              animationFillMode: "forwards",
+              animationDelay: ".7s",
+            }}
+          >
+            THE NEW ERA OF
+          </h3>
+          <h1
+            className="fade-in-top-down"
+            style={{
+              fontSize: "70px",
+              fontStyle: "bolder",
+              color: "#075BBD",
+              margin: 0,
+              opacity: 0,
+              animationFillMode: "forwards",
+              animationDelay: "1s",
+            }}
+          >
+            ROOFING
+          </h1>
+        </div>
+        <img
+          src="/assets/map.png"
+          style={{ margin: "5% auto"}}
+        />
+      </div>
     </div>
   );
 }
