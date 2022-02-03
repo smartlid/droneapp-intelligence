88518921aaee4d81e3c26770dbc18e206153418d
smartlid <lidavidwork@gmail.com>
2022-02-03 15:48:11 -0800

    Update overview page style

--- components/pages/slides/Overview.jsx
+++ components/pages/slides/Overview.jsx
@@ -46,32 +46,18 @@ export default function Product({ record, setPaginationClass }) {
       {
         <img
           src={frontview}
+          className={styles.overview__image}
           style={{
             visibility: `${side === "Front" ? "visible" : "hidden"}`,
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: -2,
-            width: "100%",
-            height: "100%",
           }}
         />
       }
       {
         <img
           src={roofview}
+          className={styles.overview__image}
           style={{
             visibility: `${side === "Roof" ? "visible" : "hidden"}`,
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: -2,
-            width: "100%",
-            height: "100%",
           }}
         />
       }
