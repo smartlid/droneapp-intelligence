0121abd4c013f36e4f35e21a1ce009a6291e354b
smartlid <lidavidwork@gmail.com>
2022-04-15 13:19:38 -0400

    Update testimonials

--- components/pages/slides/Testimonial.jsx
+++ components/pages/slides/Testimonial.jsx
@@ -58,7 +58,7 @@ export default function Testimonial() {
 
         <div className={styles["testimonial__item-container"]}>
           <div className={styles.testimonial__item}>
-            <h4 className={styles.testimonial__name}>Grady Herman</h4>
+            <h4 className={styles.testimonial__name}>Trinidad Contreras</h4>
             <div className={styles.testimonial__rating}>
               <img src="/assets/star.png" />
               <img src="/assets/star.png" />
@@ -66,13 +66,14 @@ export default function Testimonial() {
               <img src="/assets/star.png" />
               <img src="/assets/star.png" />
             </div>
-            <p>{`“We had an excellent experience working with the Remote Roofing team to have our roof and skylight replaced. They worked with our insurance adjuster to ensure our claim would cover all aspects of the roof and skylight replacement. Their consultant was great scheduling the job. The crew arrived on time and worked tirelessly until the job was completed. The project supervisor, John, came by several times during the day while the job was being completed and kept us advised of status. We had one small issue with the skylight and that was addressed and fixed promptly. The job site was left very clean. Remote Roofing Is a reputable company and we would not hesitate to recommend them.”`}</p>
-            <div
-              style={{ width: "80%", height: "200px", background: "#c4c4c4" }}
-            ></div>
+            <p>{`“Very Pleased With Bearded Brothers Roofing. Austin Helped us Fight Insurance Company after they denied our claim.. We got a brand new roof and Austin Picked a very nice color Shingle for us we love it. the crew was very professional”`}</p>
+            <img
+              src="/assets/review-trinidad-contreras.jpeg"
+              className={styles.testimonial__image}
+            />
           </div>
           <div className={styles.testimonial__item}>
-            <h4 className={styles.testimonial__name}>Bryant Barnett</h4>
+            <h4 className={styles.testimonial__name}>Kunal Patel</h4>
             <div className={styles.testimonial__rating}>
               <img src="/assets/star.png" />
               <img src="/assets/star.png" />
@@ -80,10 +81,11 @@ export default function Testimonial() {
               <img src="/assets/star.png" />
               <img src="/assets/star.png" />
             </div>
-            <p>{`“I couldn’t be happier with this company. They are so nice and professional. I had them put a roof and gutters on my rental. They did such a great job that I hired them to put a roof on the house I live in. The gutters are amazing! My neighbor asked if I would recommend them. I said yes and they roofed her house. She is happy too. I can’t recommend Remote Roofing enough. Hire them. You’ll be happy.”`}</p>
-            <div
-              style={{ width: "80%", height: "200px", background: "#c4c4c4" }}
-            ></div>
+            <p>{`“Andrea stayed in touch from start to finish and the workmanship exceeded my expectations. Great new looking roof and I am very happy to already got couple of compliments from the neighbors. Would definitely recommend Bearded Brothers.”`}</p>
+            <img
+              src="/assets/review-kuna-patel.png"
+              className={styles.testimonial__image}
+            />
           </div>
         </div>
       </div>
